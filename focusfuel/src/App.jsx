20b56e5480150561161c25acec973449b5ca9bdf
Samuel Rojas <samprogram7@gmail.com>
2025-07-18 10:57:23 -0600

    setup with vite, need to use tailwind, framer, mongodb, and express to setup

---
+++ focusfuel/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Welcome from './pages/Welcome.jsx';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Welcome />
+    </>
+  )
+}
+
+export default App
---
+++ focusfuel/src/pages/Welcome.jsx
@@ -0,0 +1,7 @@
+export default function Weclome() {
+    return (
+        <>
+        <h1>This is the welcome page</h1>
+        </>
+    )
+}
